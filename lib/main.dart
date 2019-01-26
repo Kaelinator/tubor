b8096b34cfc6af538e19cc9d99bc5357aa927b28
TimothyThompson1 <38591094+TimothyThompson1@users.noreply.github.com>
2019-01-26 17:33:26 -0600

    commit1

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'Experimental/Login.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+     
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Loginpage(),
     );
   }
 }
