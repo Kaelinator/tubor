5830d6539c7f6e5b9b68975e8440c2dbb6fb2c32
TimothyThompson1 <38591094+TimothyThompson1@users.noreply.github.com>
2019-01-27 00:11:06 -0600

    login gitfix

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-<<<<<<< HEAD
-=======
 import 'package:firebase_auth/firebase_auth.dart';
 
 >>>>>>> 804f094bba0fc64db4bcb1d330394eb20e262a68
@@ -55,48 +53,20 @@ class LoginState extends State<LoginPage> {
                           keyboardType: TextInputType.emailAddress,
                           validator: (val) => (val.isEmpty) ? 'Please enter text' : null,
                           decoration: InputDecoration(
-<<<<<<< HEAD
-                              labelText: "Enter Email",
-                              fillColor: Colors.white.withAlpha(0xde),
+                              labelText: "Email",
+                              fillColor: Colors.white,
                               ),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter an email.';
-                            }
-                          },
-=======
-                              labelText: "Email", fillColor: Colors.white),
->>>>>>> 804f094bba0fc64db4bcb1d330394eb20e262a68
                         ),
                         TextFormField(
                           controller: pass,
                           keyboardType: TextInputType.text,
                           obscureText: true,
-<<<<<<< HEAD
-                          decoration:
-                              InputDecoration(labelText: "Enter Password"),
-                          validator: (value) {
-                            if (value.isEmpty) {
-                              return 'Please enter a password.';
-                            }
-                          },
-                        ),
-                        RaisedButton(
-                            child: Text("Create Account"),
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          CreateAccount()));
-                            })
-=======
                           validator: (val) => (val.isEmpty) ? 'Please enter text' : null,
-                          decoration: InputDecoration(labelText: "Password"),
+                          decoration:
+                              InputDecoration(labelText: "Password"),
                         ),
-                        Row(
-                          children: <Widget>[
-                            RaisedButton(
+                        Row(children: <Widget>[
+                        RaisedButton(
                               child: Text("Login"),
                               onPressed: _login,
                             ),
@@ -106,12 +76,11 @@ class LoginState extends State<LoginPage> {
                                   Navigator.push(
                                       context,
                                       MaterialPageRoute(
-                                          builder: (context) =>
-                                              CreateAccount()));
-                                })
-                          ],
+                                  builder: (context) => CreateAccount()
+                                ));
+                            }
                         )
->>>>>>> 804f094bba0fc64db4bcb1d330394eb20e262a68
+                        ],)
                       ],
                     )))
               ],
