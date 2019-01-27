47b226ba55a534c102dedb35962300b98ec59da7
Kaelinator <kirkkael@gmail.com>
2019-01-27 08:43:33 -0600

    sessionlist query

--- lib/screens/SessionList.dart
+++ lib/screens/SessionList.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SessionList extends StatelessWidget {
+
+  final subjectId;
+  SessionList(this.subjectId);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance
+        .collection('events')
+        .where('subjects', arrayContains: subjectId)
+        .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError)
+          return Text('Error: ${snapshot.error}');
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting: return Text('Loading...');
+          default:
+            return ListView(
+              children: snapshot.data.documents.map((DocumentSnapshot document) {
+                return ListTile(
+                  title: Text(document['title']),
+                  subtitle: Text(document['author']),
+                );
+              }).toList(),
+            );
+        }
+      },
     );
   }
 }
