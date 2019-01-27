e446c3818419d44cae20434a02bf95e17714ffa4
Shan Memon <shankmemon@gmail.com>
2019-01-27 03:44:32 -0600

    Added date and time picker

--- lib/pages/MyProfilePage.dart
+++ lib/pages/MyProfilePage.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../widgets/CalendarPickerItem.dart';
+import 'package:flutter_calendar/flutter_calendar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import './HomePage.dart';
-
 
 class MyProfilePage extends StatelessWidget {
   final DocumentSnapshot snapshot;
@@ -13,6 +12,19 @@ class MyProfilePage extends StatelessWidget {
     FirebaseAuth.instance.signOut();
   }
 
+  void _chooseTime(BuildContext context) {
+    
+    showTimePicker(
+            context: context, 
+            initialTime: TimeOfDay.now(),
+            );
+    showDatePicker(
+            firstDate: DateTime.now(),
+            lastDate: DateTime.now()
+                .add(Duration(hours: 168, minutes: 0, seconds: 0)),
+            initialDate: DateTime.now(),
+            context: context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,9 +69,13 @@ class MyProfilePage extends StatelessWidget {
                   ),
                 ),
                 RaisedButton(
+                  child: Text("Choose Time"),
+                  onPressed: () => _chooseTime(context),
+                ),
+                RaisedButton(
                   child: Text("Sign Out"),
                   onPressed: () => _logout(context),
-                )
+                ),
               ],
             ))));
   }
--- lib/pages/TutorProfilePage.dart
+++ lib/pages/TutorProfilePage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../widgets/CalendarPickerItem.dart';
 
 class TutorProfilePage extends StatelessWidget {
   final DocumentSnapshot snapshot;
