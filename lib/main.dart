ccd100dc13e070953c4ce6de20627df954b8aee7
Dosmaster4life <jefferysuperman@gmail.com>
2019-01-26 20:48:33 -0600

    create account form

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import './pages/HomePage.dart';
 import './pages/LoginPage.dart';
-
+import './pages/CreateAccount.dart';
 bool loggedIn = false;
 
 void main() => runApp(MaterialApp(
     title: 'Flutter Demo',
     theme: ThemeData(
       primarySwatch: Colors.blue,
+      
     ),
-    home: (!loggedIn) ? LoginPage() : HomePage(title: 'hi')));
+    home: (!loggedIn) ? CreateAccount() : HomePage(title: 'hi')));
