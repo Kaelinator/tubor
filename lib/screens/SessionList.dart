a0107b007690f55109c8c46e74eb40ea5f834edd
Kaelinator <kirkkael@gmail.com>
2019-01-27 08:24:09 -0600

    populating dropdown menu

---
+++ lib/screens/SessionList.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SessionList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
