beea126e76b751173589a546221b1ddc5d82df75
TimothyThompson1 <38591094+TimothyThompson1@users.noreply.github.com>
2019-01-26 21:06:45 -0600

    Fixed login alignment & added button

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,56 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import './HomePage.dart';
+
 class LoginPage extends StatefulWidget {
-  
-@override
-State createState() => LoginState();
+  @override
+  State createState() => LoginState();
 }
 
 class LoginState extends State<LoginPage> {
-Widget build(BuildContext context) {
-return Material(
-  color: Colors.greenAccent,
- child: Scaffold(
-   backgroundColor: Colors.blueGrey,
-   body: Stack(fit: StackFit.expand, children: <Widget>[
-     
-     Image(
-       image: AssetImage("assets/kseLogo.png"),
-       fit: BoxFit.cover,
-     ),
-     Column(
-       mainAxisAlignment: MainAxisAlignment.center,
-       children: <Widget>[
-       ],
-     ),
-     Container(
-       padding: EdgeInsets.all(40),
-       child: Form(
-         child: Column(
-           children: <Widget>[
-             TextFormField(
-               keyboardType: TextInputType.emailAddress,
-               decoration: InputDecoration(labelText: "Enter Email", fillColor: Colors.white),
-             ),
-             TextFormField(
-               keyboardType: TextInputType.text,
-               obscureText: true,
-               decoration: InputDecoration(labelText: "Enter Password"),
-             )
-           ],
-         )
-       )
-     )
-
-   ],)
-
- )
-);
+  Widget build(BuildContext context) {
+    return Material(
+        color: Colors.greenAccent,
+        child: Scaffold(
+            backgroundColor: Colors.blueGrey,
+            body: Column(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(40),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image(
+                        image: AssetImage("assets/kseLogo.png"),
+                        fit: BoxFit.scaleDown,
+                        width: (MediaQuery.of(context).size.width * 0.65),
+                        height: (MediaQuery.of(context).size.width * 0.65),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                    padding: EdgeInsets.all(40),
+                    child: Form(
+                        child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              labelText: "Enter Email",
+                              fillColor: Colors.white),
+                        ),
+                        TextFormField(
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          decoration:
+                              InputDecoration(labelText: "Enter Password"),
+                        ),
+                        RaisedButton(
+                            child: Text("Create Account"),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          HomePage(title: "hi")));
+                            })
+                      ],
+                    )))
+              ],
+            )));
+  }
 }
-}
-
-
-
-
-
-
